bf837835e52c6099d639ef9ede4e526577b03528
zz <ziqizhang.email@gmail.com>
2018-04-20 08:02:02 +0100

    upload

--- code/python/src/data/twitter_collector.py
+++ code/python/src/data/twitter_collector.py
@@ -16,7 +16,6 @@ import tweepy
 from SolrClient import SolrClient
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener, Stream
-from geopy.geocoders import Nominatim
 
 
 IGNORE_RETWEETS = False
@@ -24,8 +23,6 @@ LANGUAGES_ACCETED = ["en"]
 SOLR_CORE_SEARCHAPI = "msm4phi"
 TWITTER_TIME_PATTERN = "%a %b %d %H:%M:%S %z %Y"
 SOLR_TIME_PATTERN = "%Y-%m-%dT%H:%M:%SZ"  # YYYY-MM-DDThh:mm:ssZ
-LOCATION_COORDINATES = {}  # cache to look up location geocodes
-geolocator = Nominatim()
 LOG_DIR = os.getcwd() + "/logs"
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename=LOG_DIR + '/twitter_stream.log', level=logging.INFO, filemode='w')
@@ -90,80 +87,20 @@ class TwitterStream(StreamListener):
                 time = datetime.datetime.strptime(str_created_at, TWITTER_TIME_PATTERN)
                 str_solr_time = time.utcnow().strftime(SOLR_TIME_PATTERN)
 
-
-                # place exists
-                place = jdata["place"]
-                if place is not None:
-                    place_full_name = place["full_name"]
-                    place_coordinates = place['bounding_box']['coordinates'][0][0]
-                else:
-                    place_full_name = None
-                    place_coordinates = None
-
-                coordinates = jdata["coordinates"]
-                # user_location, only compute geocode if other means have failed
-                geocode_coordinates_of_user_location = []
-                str_user_loc = jdata["user"]["location"]
-                if str_user_loc is not None and "," in str_user_loc:
-                    str_user_loc = str_user_loc.split(",")[0].strip()
-                if str_user_loc is not None and len(
-                        str_user_loc) < 25 and coordinates is None and place_full_name is None:
-                    geocode_obj = None
-                    if str_user_loc in LOCATION_COORDINATES.keys():
-                        geocode_obj = LOCATION_COORDINATES[str_user_loc]
-                    else:
-                        # geocode_obj=None #currently the api for getting geo codes seems to be unstable
-                        try:
-                            geocode_obj = self.__google_maps.search(location=str_user_loc)
-                            if (geocode_obj is not None):
-                                geocode_obj=geocode_obj.first()
-                            LOCATION_COORDINATES[str_user_loc] = geocode_obj
-                            if geocode_obj is not None:
-                                geocode_coordinates_of_user_location.append(geocode_obj.lat)
-                                geocode_coordinates_of_user_location.append(geocode_obj.lng)
-                        except Exception as exc:
-                            #traceback.print_exc(file=sys.stdout)
-                            print("\t\t gmap error={}".format(str_user_loc,exc))
-                            logger.error("\t\t gmap: {}".format(str_user_loc))
-                            try:
-                                geocode_obj = geolocator.geocode(str_user_loc)
-                                LOCATION_COORDINATES[str_user_loc] = geocode_obj
-                                if geocode_obj is not None:
-                                    geocode_coordinates_of_user_location.append(geocode_obj.latitude)
-                                    geocode_coordinates_of_user_location.append(geocode_obj.longitude)
-                            except Exception as exc:
-                                #traceback.print_exc(file=sys.stdout)
-                                print("\t\t GeoPy error={}".format(str_user_loc,exc))
-                                logger.error("\t\t GeoPy {}".format(str_user_loc))
-
-
-                if coordinates==None:
-                    coordinates=place_coordinates
-                if coordinates==None:
-                    coordinates=geocode_coordinates_of_user_location
-
-                coord_lat=None
-                coord_lon=None
-                if coordinates is not None and len(coordinates)>0:
-                    coord_lat=coordinates[0]
-                    coord_lon=coordinates[1]
-
-                docs = [{'id': jdata["id_str"],
+                doc={'id': jdata["id_str"],
                          'created_at': str_solr_time,
-                         'coordinate_lat': coord_lat,
-                         'coordinate_lon': coord_lon,
                          'lang': jdata["lang"],
-                         'place_full_name': place_full_name,
-                         'place_coordinates': place_coordinates,
-                         'status_text': jdata["text"],
-                         'user_id': jdata["user"]["id"],
-                         'user_screen_name': jdata["user"]["screen_name"],
-                         'user_statuses_count': jdata["user"]["statuses_count"],
-                         'user_friends_count': jdata["user"]["friends_count"],
-                         'user_followers_count': jdata["user"]["followers_count"],
-                         'user_location': str_user_loc,
-                         'user_location_coordinates': geocode_coordinates_of_user_location}]
-                self.__solr.index(self.__core, docs)
+                         'status_text': jdata["extended_tweet"]["full_text"]
+                         }
+                self.collect_tweet_entities(doc,jdata)
+                self.collect_tweet_quote_info(doc,jdata)
+                self.collect_retweet_info(doc,jdata)
+                self.collect_tweet_favorite_info(doc,jdata)
+                self.collect_tweet_location_info(doc,jdata)
+                self.collect_tweet_reply_info(doc,jdata)
+                self.collect_tweet_user_info(doc,jdata)
+
+                self.__solr.index(self.__core, [doc])
         except Exception as exc:
             traceback.print_exc(file=sys.stdout)
             print("Error encountered for {}, error:{} (see log file for details)".format(self.__count, exc))
@@ -190,30 +127,37 @@ class TwitterStream(StreamListener):
     def collect_tweet_entities(self, doc:dict, tweet_json:dict):
         ##################### tweet entities ###################
         # entities hashtags
-        hashtags = tweet_json["entities"]["hashtags"]
+        entities=tweet_json["extended_tweet"]["entities"]
+        hashtags = entities["hashtags"]
         hashtag_list = []
         for hashtag in hashtags:
             hashtag_list.append(hashtag["text"].lower())
 
         # entities urls
-        urls = tweet_json["entities"]["urls"]
+        urls = entities["urls"]
         url_list = []
         for url in urls:
             url_list.append(url["expanded_url"])
 
         # entities symbols
-        symbols = tweet_json["entities"]["symbols"]
+        symbols = entities["symbols"]
         symbols_list = []
         for symbol in symbols:
             symbols_list.append(symbol["text"])
 
         # entities user_mentions
-        user_mentions = tweet_json["entities"]["user_mentions"]
+        user_mentions = entities["user_mentions"]
         user_mention_list = []
         for um in user_mentions:
             user_mention_list.append(um["id"])
 
         # media todo
+        ext_entities = tweet_json["extended_tweet"]["extended_entities"]
+        if ext_entities is not None:
+            media=ext_entities["media"]
+            if media is not None:
+                doc['media_url']=media["media_url"]
+                doc['media_type']=media["media_type"]
 
         doc['entities_hashtag']= hashtag_list
         doc['entities_symbol']= symbols_list
@@ -245,6 +189,40 @@ class TwitterStream(StreamListener):
 
     def collect_tweet_favorite_info(self,doc:dict, tweet_json:dict):
         doc['favorite_count']=tweet_json["favorite_count"] #nullable
+
+    def collect_tweet_user_info(self,doc:dict, tweet_json:dict):
+        doc['user_id_str']= tweet_json["user"]["id_str"]
+        doc['user_screen_name']= tweet_json["user"]["screen_name"]
+        doc['user_statuses_count']= tweet_json["user"]["statuses_count"]
+        doc['user_friends_count']=tweet_json["user"]["friends_count"]
+        doc['user_followers_count']= tweet_json["user"]["followers_count"]
+
+    def collect_tweet_location_info(self,doc:dict, tweet_json:dict):
+        # place exists
+        place = tweet_json["place"]
+        if place is not None:
+            place_full_name = place["full_name"]
+            place_coordinates = place['bounding_box']['coordinates'][0][0]
+        else:
+            place_full_name = None
+            place_coordinates = None
+
+        coordinates = tweet_json["coordinates"]
+        # user_location, only compute geocode if other means have failed
+
+        if coordinates == None:
+            coordinates = place_coordinates
+
+        coord_lat = None
+        coord_lon = None
+        if coordinates is not None and len(coordinates) > 0:
+            coord_lat = coordinates[0]
+            coord_lon = coordinates[1]
+
+        doc['coordinate_lat']=coord_lat
+        doc['coordinate_lon']=coord_lon
+        doc['place_full_name']=place_full_name
+        doc['place_coordinates']=place_coordinates
 
 def index_data(in_file, tweepy_api, tweet_id_col, tweet_label_col):
     start=0
