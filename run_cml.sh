f5ef4caefca02cf4eeb66ea290e63f8e30e4b9c4
zz <ziqizhang.email@gmail.com>
2019-04-12 14:08:18 +0100

    script

---
+++ run_cml.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=/home/ziqi/msm4phi/code/python/src
+python3 -m exp.exp_runner /home/ziqi/data/msm4phi_data/training_data/paper_reported /home/ziqi/data/msm4phi_data/features/paper_reported /home/ziqi/msm4phi/output/classifier/raw
